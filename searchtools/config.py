25991312a5abdfda9eeb5dcb79fcf9112e817dd7
R. S. Doiel <rsdoiel@gmail.com>
2020-09-17 14:32:02 -0700

    Added tests

--- searchtools/config.py
+++ searchtools/config.py
@@ -12,9 +12,9 @@ class Configuration:
 
     def __init__(self):
         '''initialize our configuration object'''
-        self.htdocs = 'htdocs'
-        self.static = 'static'
-        self.templates = 'templates'
+        self.htdocs = ''
+        self.static = ''
+        self.templates = ''
         self.organization = ''
         self.site_title = ''
         self.site_welcome = ''
@@ -24,7 +24,7 @@ class Configuration:
         self.base_url = ''
         self.base_path = ''
         self.dataset = ''
-        self.elastic_documents_max_no = 25
+        self.elastic_documents_max_no = 0
         self.elastic_documents = ''
         self.elastic_base_endpoint = ''
         self.elastic_api_key = ''
@@ -33,6 +33,7 @@ class Configuration:
 
     def load_config(self, f_name):
         '''this reads a JSON configuration from disc and configures self'''
+        self.custom = {}
         self.config_name = f_name
         if os.path.exists(f_name):
             with open(f_name) as f:
@@ -42,54 +43,42 @@ class Configuration:
                 except Exception as err:
                     log.print(f'ERROR reading {f_name}, {err}')
                     return False
-                ok = True
-                for key in data:
-                    if 'elastic_documents_max_no' == key:
-                        self.elastic_documents_max_no = data[key]
-                    elif 'elastic_documents' == key:
-                        self.elastic_documents = data[key]
-                    elif 'elastic_base_endpoint' == key:
-                        self.elastic_base_endpoint = data[key]
-                    elif 'elastic_api_key' == key:
-                        self.elastic_api_key = data[key]
-                    elif 'elastic_engine_name' == key:
-                        self.elastic_engine_name = data[key]
-                    elif 'base_path' == key:
-                        self.base_path = data[key]
-                    elif 'base_url' == key:
-                        self.base_url = data[key]
-                    elif 'htdocs' == key:
-                        self.htdocs = data[key]
-                        if not os.path.exists(self.htdocs):
-                            log.print(f'ERROR: htdocs setting, "{self.htdocs}" from {f_name} does not exist.')
-                            ok = False
-                    elif 'static' == key:
-                        self.static = data[key]
-                        if not os.path.exists(self.static):
-                            log.print(f'ERROR: htdocs setting, "{self.static}" from {f_name} does not exist.')
-                            ok = False
-                    elif 'templates' == key:
-                        self.templates = data[key]
-                        if not os.path.exists(self.templates):
-                            log.print(f'ERROR: htdocs setting, "{self.templates}" from {f_name} does not exist.')
-                            ok = False
-                    elif 'dataset' == key:
-                        self.dataset = data[key]
-                        if not os.path.exists(self.dataset):
-                            log.print(f'ERROR: dataset collection "{self.dataset}" does not exist.')
-                            ok = False
-                    elif 'organization' == key:
-                        self.organization = data[key]
-                    elif 'site_welcome' == key:
-                        self.site_welcome = data[key]
-                    elif 'site_title' == key:
-                        self.site_title = data[key]
-                    elif 'bucket' == key:
-                        self.bucket = data[key]
-                    elif 'distribution_id' == key:
-                        self.distribution_id = data[key]
-                    else:
-                        self.custom[key] = data[key]
+            ok = True
+            for key in data:
+                if 'elastic_documents_max_no' == key:
+                    self.elastic_documents_max_no = data[key]
+                elif 'elastic_documents' == key:
+                    self.elastic_documents = data[key]
+                elif 'elastic_base_endpoint' == key:
+                    self.elastic_base_endpoint = data[key]
+                elif 'elastic_api_key' == key:
+                    self.elastic_api_key = data[key]
+                elif 'elastic_engine_name' == key:
+                    self.elastic_engine_name = data[key]
+                elif 'base_path' == key:
+                    self.base_path = data[key]
+                elif 'base_url' == key:
+                    self.base_url = data[key]
+                elif 'htdocs' == key:
+                    self.htdocs = data[key]
+                elif 'static' == key:
+                    self.static = data[key]
+                elif 'templates' == key:
+                    self.templates = data[key]
+                elif 'dataset' == key:
+                    self.dataset = data[key]
+                elif 'organization' == key:
+                    self.organization = data[key]
+                elif 'site_welcome' == key:
+                    self.site_welcome = data[key]
+                elif 'site_title' == key:
+                    self.site_title = data[key]
+                elif 'bucket' == key:
+                    self.bucket = data[key]
+                elif 'distribution_id' == key:
+                    self.distribution_id = data[key]
+                else:
+                    self.custom[key] = data[key]
         else:
             ok = False
         return ok                
@@ -103,69 +92,72 @@ class Configuration:
             value = self.elastic_documents_max_no
             if (self.elastic_documents_max_no == 0):
                 ok = False
-        if ('elastic_documents' == key):
+        elif ('elastic_documents' == key):
             value = self.elastic_documents
             if (self.elastic_documents == ''):
                 ok = False
-        if ('elastic_base_endpoint' == key):
+        elif ('elastic_base_endpoint' == key):
             value = self.elastic_base_endpoint
             if (self.elastic_base_endpoint == ''):
                 ok = False
-        if ('elastic_api_key' == key):
+        elif ('elastic_api_key' == key):
             value = self.elastic_api_key
             if (self.elastic_api_key == ''):
                 ok = False
-        if ('elastic_engine_name' == key):
+        elif ('elastic_engine_name' == key):
             value = self.elastic_engine_name
             if (self.elastic_engine_name == ''):
                 ok = False
-        if ('base_path' == key):
+        elif ('base_path' == key):
             value = self.base_path
             if (self.base_path == ''):
                 ok = False
-        if ('base_url' == key):
+        elif ('base_url' == key):
             value = self.base_url
             if (self.base_url == ''):
                 ok = False
-        if ('htdocs' == key):
+        elif ('htdocs' == key):
             value = self.htdocs
             if (self.htdocs == ''):
                 ok = False
             elif not os.path.exists(self.htdocs):
                 ok = False
-        if ('static' == key):
+        elif ('static' == key):
             value = self.static
             if (self.static == ''):
                 ok = False
             elif not os.path.exists(self.static):
                 ok = False
-        if ('templates' == key):
+        elif ('templates' == key):
             value = self.templates
             if (self.templates == ''):
                 ok = False
             elif not os.path.exists(self.templates):
                 ok = False
-        if ('dataset' == key):
+        elif ('dataset' == key):
             value = self.dataset
             if (self.dataset == ''):
                 ok = False
             elif not os.path.exists(self.dataset):
                 ok = False
-        if ('organization' == key) and (self.organization == ''):
+        elif ('organization' == key) and (self.organization == ''):
             value = self.organization
             ok = False
-        if ('site_welcome' == key) and (self.site_welcome == ''):
+        elif ('site_welcome' == key) and (self.site_welcome == ''):
             value = site_welcome
             ok = False
-        if ('site_title' == key) and (self.site_title == ''):
+        elif ('site_title' == key) and (self.site_title == ''):
             value = site_title
             ok = False
-        if ('bucket' == key) and (self.bucket == ''):
+        elif ('bucket' == key) and (self.bucket == ''):
             value = self.bucket
             ok = False
-        if ('distribution_id' == key) and (self.distribution_id == ''):
+        elif ('distribution_id' == key) and (self.distribution_id == ''):
             value = self.distribution_id
             ok = False
+        else:
+            ok = False
+        # NOTE: Return the value found if set, otherwise return default
         if ok:
             return value
         return default
@@ -175,76 +167,79 @@ class Configuration:
         f_name = self.config_name
         ok = True
         for setting in settings:
-            if ('elastic_documents_max_no' in settings):
+            if ('elastic_documents_max_no' == setting):
                 if (self.elastic_documents_max_no == 0):
                     print(f'elastic_documents_max_no not set in {f_name}')
                     ok = False
-            if ('elastic_documents' in settings):
+            elif ('elastic_documents' == setting):
                 if (self.elastic_documents == ''):
                     print(f'elastic_documents not set in {f_name}')
                     ok = False
-            if ('elastic_base_endpoint' in settings):
+            elif ('elastic_base_endpoint' == setting):
                 if (self.elastic_base_endpoint == ''):
                     print(f'elastic_base_endpoint not set in {f_name}')
                     ok = False
-            if ('elastic_api_key' in settings):
+            elif ('elastic_api_key' == setting):
                 if (self.elastic_api_key == ''):
                     print(f'elastic_api_key not set in {f_name}')
                     ok = False
-            if ('elastic_engine_name' in settings):
+            elif ('elastic_engine_name' == setting):
                 if (self.elastic_engine_name == ''):
                     print(f'elastic_engine_name not set in {f_name}')
                     ok = False
-            if ('base_path' in settings):
+            elif ('base_path' == setting):
                 if (self.base_path == ''):
                     print(f'base_path not set in {f_name}')
                     ok = False
-            if ('base_url' in settings):
+            elif ('base_url' == setting):
                 if (self.base_url == ''):
                     print(f'base_url not set in {f_name}')
                     ok = False
-            if ('htdocs' in settings):
+            elif ('htdocs' == setting):
                 if (self.htdocs == ''):
                     print(f'htdocs not set in {f_name}')
                     ok = False
                 elif not os.path.exists(self.htdocs):
                     print(f'htdocs {self.htdocs} does not exist.')
                     ok = False
-            if ('static' in settings):
+            elif ('static' == setting):
                 if (self.static == ''):
                     print(f'static not set in {f_name}')
                     ok = False
                 elif not os.path.exists(self.static):
                     print(f'static {self.static} does not exist.')
                     ok = False
-            if ('templates' in settings):
+            elif ('templates' == setting):
                 if (self.templates == ''):
                     print(f'templates not set in {f_name}')
                     ok = False
                 elif not os.path.exists(self.templates):
                     print(f'templates {self.templates} does not exist.')
                     ok = False
-            if ('dataset' in settings):
+            elif ('dataset' == setting):
                 if (self.dataset == ''):
                     print(f'dataset not set in {f_name}')
                     ok = False
                 elif not os.path.exists(self.dataset):
                     print(f'dataset {self.dataset} does not exist.')
                     ok = False
-            if ('organization' in settings) and (self.organization == ''):
+            elif ('organization' == setting) and (self.organization == ''):
                 print(f'organization not set in {f_name}')
                 ok = False
-            if ('site_welcome' in settings) and (self.site_welcome == ''):
+            elif ('site_welcome' == setting) and (self.site_welcome == ''):
                 print(f'site_welcome not set in {f_name}')
                 ok = False
-            if ('site_title' in settings) and (self.site_title == ''):
+            elif ('site_title' == setting) and (self.site_title == ''):
                 print(f'site_title not set in {f_name}')
                 ok = False
-            if ('bucket' in settings) and (self.bucket == ''):
+            elif ('bucket' == setting) and (self.bucket == ''):
                 print(f'bucket not set in {f_name}')
                 ok = False
-            if ('distribution_id' in settings) and (self.distribution_id == ''):
+            elif ('distribution_id' == setting) and (self.distribution_id == ''):
                 print(f'distribution_id not set in {f_name}')
+                ok = False
+            elif not (setting in self.custom):
+                print(f'{setting} not set in {f_name}')
                 ok = False
         return ok                
 
---
+++ searchtools_test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env Python3
+
+import os
+import sys
+
+# package to test
+import searchtools 
+
+
+def test_logger():
+    ok = True
+    log = searchtools.Logger(os.getpid())
+    msg = 'This is a log message'
+    if __debug__: 
+        print(f'Expecting "{msg}" with a timestamp and pid prefix')
+        log.print(msg)
+    s = log.sprint(msg)
+    if not (msg in s):
+        print(f'Expected "{msg}" in s: "{s}"')
+        ok = False
+    return ok
+
+def test_empty_config():
+    ok = True
+    cfg = searchtools.Configuration()
+    # NOTE: cfg has been created but not loaded yet.
+    organization = cfg.get('organization', False)
+    if organization != False:
+        print(f'Expected False for organization, got "{organization}"')
+        ok = False
+    return ok
+
+def test_config():
+    ok = True
+    f_name = os.path.join("tests", "test_config.json")
+    cfg = searchtools.Configuration()
+    if not cfg.load_config(f_name):
+        print(f'Failed to load {f_name}')
+        ok = False
+    keys = [ "elastic_documents" ]
+    if not cfg.required(keys):
+        print(f'Expected "{", ".join(keys)}", required = False')
+        ok = False
+    print(f'Expecting warning about missing "beanstalk" on {f_name}')
+    keys = [ "beanstalk" ]
+    expected = False
+    value = cfg.required(keys)
+    if expected != value:
+        print(f'Expected {expected}, got {value} for keys "{keys}"')
+        print(cfg.toJSON())
+        ok = False
+    key = "elastic_documents"
+    expected = True
+    value = cfg.get(key, False)
+    if not (value == 'elastic-documents.json'):
+        print(f'Expected "elastic-documents.json", got "{value}" for {key}')
+        #print(cfg.toJSON())
+        ok = False
+    key = 'beanstalk'
+    value = cfg.get(key, "Hello World")
+    if value != "Hello World":
+        print(f'Expected (default) "Hello World", got "{value}" for {key}')
+        print(cfg.toJSON())
+        ok = False
+    return ok
+
+if __name__ == '__main__':
+    if not test_logger():
+        print('test_logger() failed.')
+        sys.exit(1)
+    if not test_empty_config():
+        print('test_empty_config() failed.')
+        sys.exit(1)
+    if not test_config():
+        print('test_config() failed.')
+        sys.exit(1)
+    print('OK')
+    sys.exit(0)
